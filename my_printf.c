dfa3dd26b2c639aaf31337b407c2e635b91f5c69
sixthson6 <sudais2408@gmai.com>
2023-08-21 00:48:25 -0700

    updated

--- my_printf.c
+++ my_printf.c
@@ -1,21 +1,80 @@
 #include "main.h"
 
 /**
- * _printf - custom printf() func
+ * print_str - print string
+ * @str: string
+ * Return: Nothing
+ */
+
+void print_str(char *str)
+{
+	if (str == NULL)
+		return;
+
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+}
+
+/**
+ * printNumber - print num
+ * @num: number
+ * Return: Nothing
+ */
+
+void printNumber(int num)
+{
+	int revNum, digit;
+
+	if (num < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+
+	if (num == 0)
+	{
+		putchar('0');
+		return;
+	}
+
+	revNum = 0;
+
+	while (num > 0)
+	{
+		digit = num % 10;
+		revNum = revNum * 10 + digit;
+		num /= 10;
+	}
+
+	while (revNum > 0)
+	{
+		digit = revNum % 10;
+		putchar('0' + digit);
+		revNum /= 10;
+	}
+}
+
+
+/**
+ * _printf - custom printf()
  * @format: format string
- * Return: 0 (Success)
+ * Return: 0 (Success);
  */
 
 int _printf(const char *format, ...)
 {
 	va_list arguments;
-	int count = 0;
 	int letter;
 	char *str;
+	int count = 0;
+	int num;
 
 	va_start(arguments, format);
 
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
@@ -28,23 +87,21 @@ int _printf(const char *format, ...)
 					putchar(letter);
 					count++;
 					break;
-
 				case 's':
 					str = va_arg(arguments, char *);
-					while (*str != '\0')
-					{
-						putchar(*str);
-						str++;
-						count++;
-					}
+					print_str(str);
+					count++;
 					break;
-
 				case '%':
 					putchar('%');
 					count++;
 					break;
-
-
+				case 'd':
+				case 'i':
+					num = va_arg(arguments, int);
+					printNumber(num);
+					count++;
+					break;
 				default:
 					putchar('%');
 					putchar(*format);
@@ -63,5 +120,5 @@ int _printf(const char *format, ...)
 
 	va_end(arguments);
 
-	return (0);
+	return (count);
 }
